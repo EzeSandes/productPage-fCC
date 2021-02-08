65194bfd87a5d1fc373c83d33ddce8d7739d9397
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2021-02-08 08:34:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+/* MANTENER BARRA DE NAVEGACION FIJA MIENTRAS NAVEGO POR LA PAGINA */
+
+window.onscroll = function () { stickyNavbar() };
+
+// Get the navbar
+const navbar = document.getElementById("nav-bar");
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNavbar() {
+   if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+   } else {
+      navbar.classList.remove("sticky");
+   }
+}
